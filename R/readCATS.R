ed1c459beab804cc48d9f9f80bb9d2aec8722047
Martin Biuw <martin.biuw@hi.no>
2020-11-25 18:50:46 +0100

    Addded GPS reading function

--- R/readCATS.R
+++ R/readCATS.R
@@ -1,5 +1,5 @@
 #' \code{readDiary} Reading csv file of CATS diary data
-#' @param filename NAme of file to be read (can be \code{file.choose()} to interactivelo select file)
+#' @param filename Name of file to be read (can be \code{file.choose()} to interactivelo select file)
 #' @details Reads standard diary data collected by CATS data loggers, saved as a csv file
 #' @returns Returns a tibble with all data stored in file
 #' @family CATS data reading and manipulation functions
@@ -8,9 +8,10 @@
 #' dat <- readDiary(file.choose())
 #'
 #' @imports tidyverse
-#' @export
+#' @imports readr
+#' #' @export
 
-readDiary <- function(filename) {
+readDiary <- function(filename=file.choose()) {
   require(tidyverse)
   diary <- readr::read_csv(filename)
   names(diary) <- unlist(lapply(names(diary), function(x) {
@@ -26,5 +27,36 @@ readDiary <- function(filename) {
   diary$DT_UTC <- as.POSIXct(strptime(paste(diary$Date_UTC, 
                                             diary$Time_UTC), '%d.%m.%Y %H:%M:%S'),
                              tz='UTC')
- diary 
+  sampFreq <- round(length(dat$DT_UTC)/length(unique(dat$DT_UTC)))
+  diary 
+}
+
+
+#' \code{readGps} Reading csv file of CATS gps data
+#' @param filename Name of file to be read (can be \code{file.choose()} to interactivelo select file)
+#' @details Reads standard gps data collected by CATS data loggers, saved as a csv file
+#' @returns Returns a tibble with all data stored in file
+#' @family CATS data reading and manipulation functions
+#' @author Martin Biuw
+#' @examples
+#' gps <- readGPS(file.choose())
+#'
+#' @imports tidyverse
+#' @imports readr
+#' @export
+
+readGPS <- function(filename=file.choose()) {
+  require(tidyverse)
+  gps <- readr::read_csv(filename)
+  names(gps) <- unlist(lapply(names(gps), function(x) {
+    paste(unlist(strsplit(x, ' '))[c(1,2)], collapse='_')
+  }))
+  names(gps) <- gsub('_NA', '', names(gps))
+  names(gps) <- gsub('(', '', names(gps), fixed=T)
+  names(gps) <- gsub(')', '', names(gps), fixed=T)
+  
+  gps$DT_UTC <- as.POSIXct(strptime(paste(gps$Date_GPS, 
+                                            gps$Time_GPS), '%d.%m.%Y %H:%M:%S'),
+                             tz='UTC')
+  gps
 }
